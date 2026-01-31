4db110df10c2c694fc093c110521b85fbe2b307f
Adarsh Singh <adarshsikarwar543@gmail.com>
2026-02-01 04:31:11 +0530

    second commit

--- 2/src/App.jsx
+++ 2/src/App.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import { useState } from "react";
 
 const App = () => {
-  const [val, setval] = useState(false);
-  let changeState = () => !val;
+  const [val, setval] = useState([1, 2, 3, 4, 5, 6]);
+  console.log(val);
   return (
     <div className="h-screen w-full bg-blue-200 p-3 flex flex-col justify-center items-center gap-7">
-      <h1 className="uppercase text-5xl ">{val.toString()}</h1>
+      <div className="w-[27%] bg-red-50 p-3 flex flex-col items-center gap-3 ">
+        {val.map((ele) => (
+          <h1 className="uppercase text-2xl ">{ele}</h1>
+        ))}
+      </div>
       <button
-        onClick={() => setval(!val)}
+        onClick={() => setval((prev) => prev.filter((ele, id) => ele % 2))}
         className="w-fit px-10 py-1 border text-3xl text-[#c26565] bg-amber-200"
       >
         Click
