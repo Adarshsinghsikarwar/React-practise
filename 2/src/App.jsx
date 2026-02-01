ad6f0052fe34eaa675debaa44cb1633baa41df3a
Adarsh Singh <adarshsikarwar543@gmail.com>
2026-02-01 23:52:30 +0530

    task 2 complete

--- 2/src/App.jsx
+++ 2/src/App.jsx
@@ -2,17 +2,33 @@ import React from "react";
 import { useState } from "react";
 
 const App = () => {
-  const [val, setval] = useState([1, 2, 3, 4, 5, 6]);
+  const [val, setval] = useState([
+    { name: "Adarsh Singh", email: "adarshsingh@gmail.com" },
+    { name: "Devansh Pal", email: "devanshpal@gmail.com" },
+    { name: "Rahul chauhan", email: "rahulchauhan@gmail.com" },
+    { name: "Shubham chota", email: "shubhamchota@gmail.com" },
+  ]);
   console.log(val);
   return (
     <div className="h-screen w-full bg-blue-200 p-3 flex flex-col justify-center items-center gap-7">
       <div className="w-[27%] bg-red-50 p-3 flex flex-col items-center gap-3 ">
         {val.map((ele) => (
-          <h1 className="uppercase text-2xl ">{ele}</h1>
+          <div>
+            <h1 className="uppercase text-2xl ">Name : {ele.name}</h1>
+            <h1 className="uppercase text-2xl ">Email :{ele.email}</h1>
+          </div>
         ))}
       </div>
       <button
-        onClick={() => setval([...val, 8])}
+        onClick={() =>
+          setval((prev) =>
+            prev.map((ele, id) =>
+              ele.name === "Adarsh Singh"
+                ? { ...ele, email: "kallusingh@gmail.com" }
+                : ele
+            )
+          )
+        }
         className="w-fit px-10 py-1 border text-3xl text-[#c26565] bg-amber-200"
       >
         Click
