46d593f1bffccd1c971deaedba7dc2f63c0053dc
Adarsh Singh <adarshsikarwar543@gmail.com>
2026-02-02 22:07:52 +0530

    task5 second commit

--- 5/src/App.jsx
+++ 5/src/App.jsx
@@ -1,27 +1,23 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  const name = useRef(null);
-  const age = useRef(null);
-  const email = useRef(null);
+  const [val, setVal] = useState({ name: "" });
+  console.log(val);
 
   const submitForm = (event) => {
     event.preventDefault();
-
-    console.log(name.current.value);
-    console.log(age.current.value);
-    console.log(email.current.value);
   };
 
   return (
-    <form 
-      onSubmit={submitForm}
-      className="h-screen w-full bg-blue-100 p-10"
-    >
-      <input ref={name} placeholder="name" type="text border-b-4" />
-      <input ref={age} placeholder="age" type="text" />
-      <input ref={email} placeholder="email" type="text" />
-      <input type="submit" className="px-3 py-1 bg-red-200 text-2xl"/>
+    <form onSubmit={submitForm} className="h-screen w-full bg-blue-100 p-10">
+      <input
+        value={val.name}
+        onChange={(event) => setVal({ ...val, name: event.target.value })}
+        placeholder="name"
+        type="text"
+        className="border-b-4"
+      />
+      <input type="submit" className="px-3 py-1 bg-red-200 text-2xl" />
     </form>
   );
 };
